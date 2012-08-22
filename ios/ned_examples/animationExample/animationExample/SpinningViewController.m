bdea8b5a2c88ac0247a57901270cec3adcfece1d
Hugo Melo <squantowalks@gmail.com>
2012-08-22 12:34:03 -0700

    Progress

--- ios/ned_examples/animationExample/animationExample/SpinningViewController.m
+++ ios/ned_examples/animationExample/animationExample/SpinningViewController.m
@@ -21,15 +21,23 @@
 
 // CAAnimcation class describes how to animate things
 -(CAAnimation *)spinAnimation {
-    //
+    // This is keyvalue coding. You can add other things like position.
     CABasicAnimation *spinAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
     
     // Each basic animation has a from value and a to value.
     spinAnimation.fromValue = [NSNumber numberWithDouble:0];
-    spinAnimation.fromValue = [NSNumber numberWithDouble:(2 * M_PI)];
-    spinAnimation.duration = 2.0;
+    spinAnimation.toValue = [NSNumber numberWithDouble:(2 * M_PI)];
+    spinAnimation.duration = 3.0;
     
     return spinAnimation;
+}
+
+-(CAAnimation *)slideAnimation {
+    CABasicAnimation *slideAnimation = [CABasicAnimation animationWithKeyPath:@"position"];
+    slideAnimation.fromValue = [NSValue valueWithCGPoint:CGPointZero];
+    slideAnimation.toValue = [NSValue valueWithCGPoint:CGPointMake(100, 400)];
+    slideAnimation.duration = 4.0;
+    return slideAnimation;
 }
 
 
@@ -37,6 +45,7 @@
     // I want the animation self SpinAnimation to start for this layer.
     // Key is just for us.
     [self.labelToSpin.layer addAnimation:[self spinAnimation] forKey:@"SpinAnimation"];
+    [self.labelToSpin.layer addAnimation:[self slideAnimation] forKey:@"SlideAnimation"];
     [self.buttonToSpin.layer addAnimation:[self spinAnimation] forKey:@"SpinAnimation"];
 }
 
