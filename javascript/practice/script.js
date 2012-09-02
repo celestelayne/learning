55d3466f5b79025316da0bff2f5d76c94fe017c2
Hugo Melo <squantowalks@gmail.com>
2012-09-01 22:31:50 -0700

    Javascript Work

---
+++ javascript/practice/script.js
@@ -0,0 +1,12 @@
+// var sentence = "This is one sentence. This is a sentence with a list of items: cherries, oranges, apples, bananas.";
+// var start = sentence.indexOf(":");
+// var end = sentence.indexOf(".", start+1);
+// 
+// var list = sentence.substring(start+1, end);
+// var fruits = list.split(",")
+// 
+// if(((typeof list != "undefined") && (typeof list.valueOf() == "string")) && (list.length > 0)) {
+// 	console.log("The List is good to go:" + list);
+// }
+
+
